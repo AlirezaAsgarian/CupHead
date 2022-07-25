710817c9bf54fa9c02146f855c4790ca9a1a3777
Alireza Askarian <ar.asgarian1389@gmail.com>
2022-07-25 12:08:53 +0430

    Game class added

---
+++ src/main/java/MainModule/Model/Game.java
@@ -0,0 +1,17 @@
+package MainModule.Model;
+
+import MainModule.Util.BossBirdStack;
+
+public class Game {
+
+    Avatar avatar;
+    BossBirdStack bossBirdStack;
+
+    public Game(Avatar avatar, BossBirdStack bossBirdStack) {
+        this.avatar = avatar;
+        this.bossBirdStack = bossBirdStack;
+    }
+    public void startNewGame(){
+
+    }
+}
--- src/main/java/MainModule/Util/BossBirdStack.java
+++ src/main/java/MainModule/Util/BossBirdStack.java
@@ -6,9 +6,17 @@ import MainModule.Model.BossBirdEnums;
 import java.util.Stack;
 
 public class BossBirdStack {
-    public final static Stack<BossBird> bossBirdStack = new Stack<BossBird>() {{
+    public static Stack<BossBird> bossBirdStack = new Stack<BossBird>() {{
         push(BossBirdEnums.THIRD_BOSS_BIRD.getBossBird());
         push(BossBirdEnums.SECOND_BOSS_BIRD.getBossBird());
         push(BossBirdEnums.FIRST_BOSS_BIRD.getBossBird());
     }};
+
+    public static void setBossBirdStack(Stack<BossBird> bossBirdStack) {
+        BossBirdStack.bossBirdStack = bossBirdStack;
+    }
+
+    public static Stack<BossBird> getBossBirdStack() {
+        return bossBirdStack;
+    }
 }
